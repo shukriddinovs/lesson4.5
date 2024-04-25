c3a6ddd8cf7663d05af5f0446140dd003d8f508c
shukriddinovs <sshukriddinov524@gmail.com>
2024-04-25 14:27:03 +0500

    {.}

---
+++ main.js
@@ -0,0 +1,10 @@
+let name1 =  prompt("ismingizni kiriting")
+let test = prompt("harf yozing")
+
+
+if (test) {
+alert("sizning ismingizda bu harf mavjud")
+    
+}else{
+alert("sizning ismingizda bu harf mavjud emas")
+}
